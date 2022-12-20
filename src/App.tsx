c16a1bb169ffcf87634edad8a1d27d81e3d2c411
DANG NGOC HIEU <hieudn@ominext.com>
2022-12-20 18:24:05 +0700

    coding

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <Fragment>
+      <h1 className='font-bold text-red-500'>This is Anfield 🔥 </h1>
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/components/Input/entity.ts
@@ -0,0 +1,9 @@
+export interface InputProps {
+  type: 'input' | 'textArea'
+  value: string
+  placeholder?: string
+  label?: string
+  prefixIcon?: string
+  suffixIcon?: string
+  inputType?: string
+}
